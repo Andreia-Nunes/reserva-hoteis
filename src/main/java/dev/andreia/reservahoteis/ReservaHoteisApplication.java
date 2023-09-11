28998b86ce953862370429c2bbf7933209854453
Andreia-Nunes <andreia.nunes@academico.ifpb.edu.br>
2023-09-10 21:25:57 -0300

    Criação do projeto Spring Boot

---
+++ src/main/java/dev/andreia/reservahoteis/ReservaHoteisApplication.java
@@ -0,0 +1,13 @@
+package dev.andreia.reservahoteis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaHoteisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaHoteisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/andreia/reservahoteis/ReservaHoteisApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andreia.reservahoteis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaHoteisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
