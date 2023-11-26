4383676700aa4335be6dcaa5a1ba9d822e2dbf3d
Andreia-Nunes <andreia.nunes@academico.ifpb.edu.br>
2023-11-26 19:14:11 -0300

    Adição do método de validação do token na classe TokenService

--- src/main/java/dev/andreia/reservahoteis/security/TokenService.java
+++ src/main/java/dev/andreia/reservahoteis/security/TokenService.java
@@ -3,6 +3,7 @@ package dev.andreia.reservahoteis.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import dev.andreia.reservahoteis.model.security.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -30,6 +31,21 @@ public class TokenService {
         } catch(JWTCreationException exception){
             throw new RuntimeException("Error while generating token", exception);
         }
+    }
+
+    public String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(this.secret);
+            return JWT.require(algorithm)
+                    .withIssuer("reserva-hoteis")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        } catch(JWTVerificationException exception){
+            return "";
+        }
+
     }
 
     private Instant genExpirationDate(){
