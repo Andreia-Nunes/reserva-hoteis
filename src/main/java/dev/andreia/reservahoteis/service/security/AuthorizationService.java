d9ef9e6f949cebb88969d59c3f5ac7507613e0f6
Andreia-Nunes <andreia.nunes@academico.ifpb.edu.br>
2023-11-26 17:00:42 -0300

    Criação do Service de autenticação

---
+++ src/main/java/dev/andreia/reservahoteis/service/security/AuthorizationService.java
@@ -0,0 +1,23 @@
+package dev.andreia.reservahoteis.service.security;
+
+import dev.andreia.reservahoteis.repository.security.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    public AuthorizationService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
