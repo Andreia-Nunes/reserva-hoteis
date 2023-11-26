cfac921beecfff628d360853a07abbf4ee3e2dae
Andreia-Nunes <andreia.nunes@academico.ifpb.edu.br>
2023-11-26 17:32:36 -0300

    Configuração da autenticação das requests através de matchers e roles

--- src/main/java/dev/andreia/reservahoteis/security/SecurityConfigurations.java
+++ src/main/java/dev/andreia/reservahoteis/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package dev.andreia.reservahoteis.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,10 @@ public class SecurityConfigurations {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/hotel").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
                 .build();
     }
 }
